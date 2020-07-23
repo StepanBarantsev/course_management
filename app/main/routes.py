addda2d8c1668fba024b28071d3a11a171df8c07
Баранцев Степан <stepan.barantsev@gmail.com>
2020-07-23 22:59:24 +0300

    Базовый шаблон для главной страницы

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,9 +1,10 @@
 from app.main import bp
 from flask_login import login_required
+from flask import render_template
 
 
 @bp.route('/', methods=['GET'])
 @bp.route('/index', methods=['GET'])
 @login_required
 def index():
-    return 'Привет мир!'
+    return render_template('main/index.html', title='Авторизация')
