eca7f52f7e4ade859ffb290ff5aa7c2671cb726f
Баранцев Степан <stepan.barantsev@gmail.com>
2020-07-30 02:29:21 +0300

    Кнопка редактирования профиля

--- app/profile/routes.py
+++ app/profile/routes.py
@@ -3,8 +3,13 @@ from flask_login import login_required
 from flask import render_template
 
 
-@bp.route('/', methods=['GET'])
 @bp.route('/profile', methods=['GET'])
 @login_required
 def profile():
     return render_template('profile/profile.html', title='Профиль')
+
+
+@bp.route('/editprofile', methods=['GET'])
+@login_required
+def editprofile():
+    return render_template('profile/edit.html', title='Редактирование профиля')
