b94fc43db8e321436802a7cba1a020ba7c61921c
Баранцев Степан <stepan.barantsev@gmail.com>
2020-08-16 03:59:44 +0300

    Редактирование курса

--- app/coursecreate/forms.py
+++ app/coursecreate/forms.py
@@ -16,7 +16,23 @@ class CreateCourseProfileForm(FlaskForm):
 
     def validate_name(self, name):
         coursename = db.session.query(Course).filter(Course.name == name.data).filter(Course.user_id == self.current_user.id).first()
-        print(db.session.query(Course).filter(Course.name == name.data).filter(Course.user_id == self.current_user.id))
         if coursename is not None:
             raise ValidationError('Данное имя курса уже занято!')
+
+
+class EditCourseProfileForm(FlaskForm):
+
+    name = StringField('Название курса', validators=[DataRequired()])
+    submit = SubmitField('Сохранить', render_kw={'class': "btn btn-success"})
+
+    def __init__(self, current_user, old_course_name, *args, **kwargs):
+        super(EditCourseProfileForm, self).__init__(*args, **kwargs)
+        self.current_user = current_user
+        self.old_course_name = old_course_name
+
+    def validate_name(self, name):
+        coursename = db.session.query(Course.name).filter(Course.name == name.data).filter(Course.user_id == self.current_user.id).first()
+        if coursename is not None and coursename.name != self.old_course_name:
+            raise ValidationError('Данное имя курса уже занято!')
+
 
--- app/coursecreate/routes.py
+++ app/coursecreate/routes.py
@@ -1,10 +1,11 @@
 from flask_login import login_required
 from app.coursecreate import bp
 from app.models import Course
-from app.coursecreate.forms import CreateCourseProfileForm
+from app.coursecreate.forms import CreateCourseProfileForm, EditCourseProfileForm
 from flask_login import current_user
 from app import db
 from flask import render_template, redirect, url_for, flash, request
+from sqlalchemy import update
 
 
 @bp.route('/create', methods=['GET', 'POST'])
@@ -17,4 +18,20 @@ def create():
         db.session.commit()
         flash('Новый курс был успешно создан!')
         return redirect(url_for('main.index'))
-    return render_template('coursecreate/create.html', title='Создать курс', form=form)
+    return render_template('coursecreate/editcreate.html', title='Создать курс', form=form)
+
+
+@bp.route('/edit', methods=['GET', 'POST'])
+@login_required
+def edit():
+    course_id = request.args.get('course_id', 1, type=int)
+    course = db.session.query(Course.name).filter(Course.id == course_id).first()
+    form = EditCourseProfileForm(current_user, course.name)
+    if form.validate_on_submit():
+        db.session.execute(update(Course).where(Course.id == course_id).values(name=form.name.data))
+        db.session.commit()
+        flash('Данные курса были успешно изменены!')
+        return redirect(url_for('main.index'))
+    elif request.method == 'GET':
+        form.name.data = course.name
+    return render_template('coursecreate/editcreate.html', title='Редактировать курс', form=form)
