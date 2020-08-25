3c518ea2141ccbd51595435b22cb9865012c3045
Баранцев Степан <stepan.barantsev@gmail.com>
2020-08-26 00:23:58 +0300

    Убрал связь много к много

--- app/models.py
+++ app/models.py
@@ -44,12 +44,6 @@ class User(db.Model, UserMixin):
         return self.get_all_not_deleted_courses().filter_by(lms_id=lms_id).first()
 
 
-student_course = db.Table('student_course', db.Model.metadata,
-    db.Column('student_id', db.Integer, db.ForeignKey('students.id')),
-    db.Column('course_id', db.Integer, db.ForeignKey('courses.id'))
-)
-
-
 class Course(db.Model):
     __tablename__ = 'courses'
     id = db.Column(db.Integer(), primary_key=True)
@@ -66,7 +60,7 @@ class Course(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
     blocks = db.relationship('CourseBlock', backref='course', lazy='dynamic')
-    students = db.relationship("Student", secondary=student_course)
+    students = db.relationship('Student', backref='course', lazy='dynamic')
 
     def __repr__(self):
         return '<Course {}>'.format(self.body)
@@ -93,7 +87,7 @@ class CourseBlock(db.Model):
 class Student(db.Model):
     __tablename__ = 'students'
     id = db.Column(db.Integer(), primary_key=True)
-    username = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False, unique=True)
     freezed = db.Column(db.Boolean(), nullable=False, default=False)
     number_of_days = db.Column(db.Integer(), nullable=False)
@@ -102,7 +96,7 @@ class Student(db.Model):
     telegram_id = db.Column(db.Integer())
     deleted = db.Column(db.Boolean(), nullable=False, default=False)
 
-    courses = db.relationship("Course", secondary=student_course)
+    course_id = db.Column(db.Integer, db.ForeignKey('courses.id'), nullable=False)
 
     @staticmethod
     def delete_student_by_id(student_id):
