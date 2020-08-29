5e39d8c3f2fa4b19dec6d3f6879bcc38407170be
Баранцев Степан <stepan.barantsev@gmail.com>
2020-08-29 22:57:50 +0300

    Update lms_api_helper.py

--- api_helper/lms_api_helper.py
+++ api_helper/lms_api_helper.py
@@ -4,7 +4,7 @@ import api_helper.config
 class LmsApiHelper:
 
     def __init__(self):
-        self.lms_key = api_helper.config
+        self.lms_key = api_helper.config.ConfigApi.lms_key
 
     def get_student_by_lms_id(self):
         pass
