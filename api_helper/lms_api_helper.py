209c2b4a0348a6dfa8c1fa871a3481f8cc5c2760
Баранцев Степан <stepan.barantsev@gmail.com>
2020-09-04 23:15:01 +0300

    Метод получения инфы о студенте

--- api_helper/lms_api_helper.py
+++ api_helper/lms_api_helper.py
@@ -1,4 +1,5 @@
 import api_helper.config
+import requests
 
 
 class LmsApiHelper:
@@ -6,5 +7,6 @@ class LmsApiHelper:
     def __init__(self):
         self.lms_key = api_helper.config.ConfigApi.lms_key
 
-    def get_student_by_lms_id(self):
-        pass
+    @staticmethod
+    def get_student_by_lms_id(self, lms_id):
+        requests.get(f'https://software-testing.ru/lms/webservice/rest/server.php?wstoken={self.lms_key}&wsfunction=core_user_get_users&criteria[0][key]=id&criteria[0][value]={lms_id}&moodlewsrestformat=json')
