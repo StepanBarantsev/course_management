c9811c7779ca0e3a8a911e63620c795e2bf059e1
Баранцев Степан <stepan.barantsev@gmail.com>
2020-09-04 23:19:17 +0300

    Сделал класс статикой + получаем из метода словарь

--- api_helper/lms_api_helper.py
+++ api_helper/lms_api_helper.py
@@ -1,12 +1,13 @@
 import api_helper.config
 import requests
+import json
 
 
 class LmsApiHelper:
 
-    def __init__(self):
-        self.lms_key = api_helper.config.ConfigApi.lms_key
+    lms_key = api_helper.config.ConfigApi.lms_key
 
     @staticmethod
-    def get_student_by_lms_id(self, lms_id):
-        requests.get(f'https://software-testing.ru/lms/webservice/rest/server.php?wstoken={self.lms_key}&wsfunction=core_user_get_users&criteria[0][key]=id&criteria[0][value]={lms_id}&moodlewsrestformat=json')
+    def get_student_by_lms_id(lms_id):
+        return json.loads(requests.get(f'https://software-testing.ru/lms/webservice/rest/server.php?wstoken={LmsApiHelper.lms_key}&wsfunction=core_user_get_users&criteria[0][key]=id&criteria[0][value]={lms_id}&moodlewsrestformat=json').text)
+
