3e8d12e987488e5e18dae7f9ffef107ce4be1664
Баранцев Степан <stepan.barantsev@gmail.com>
2020-09-04 23:45:18 +0300

    Изменил метод вытягивания инфы о студенте

--- api_helper/lms_api_helper.py
+++ api_helper/lms_api_helper.py
@@ -9,5 +9,5 @@ class LmsApiHelper:
 
     @staticmethod
     def get_student_by_lms_id(lms_id):
-        return json.loads(requests.get(f'https://software-testing.ru/lms/webservice/rest/server.php?wstoken={LmsApiHelper.lms_key}&wsfunction=core_user_get_users&criteria[0][key]=id&criteria[0][value]={lms_id}&moodlewsrestformat=json').text)
+        return json.loads(requests.get(f'https://software-testing.ru/lms/webservice/rest/server.php?wstoken={LmsApiHelper.lms_key}&wsfunction=core_user_get_users&criteria[0][key]=id&criteria[0][value]={lms_id}&moodlewsrestformat=json').text)['users'][0]
 
