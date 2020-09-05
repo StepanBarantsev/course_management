c2e0cc1435d3978496eb1e537c43b7a576ef8f0b
Баранцев Степан <stepan.barantsev@gmail.com>
2020-09-05 11:48:33 +0300

    Запрет редачить чужие курсы

--- web/app/coursecreate/routes.py
+++ web/app/coursecreate/routes.py
@@ -52,41 +52,45 @@ def create():
 def edit():
     course_id = request.args.get('course_id', 1, type=int)
     course = db.session.query(Course).filter(Course.id == course_id).first()
-    form = CreateOrEditCourseForm(current_user, course.name, course.lms_id)
-    if form.validate_on_submit():
 
-        if not form.is_more_then_one_block.data:
-            num_of_blocks = 1
-        else:
-            if form.number_of_blocks.data is None:
+    if course.author.id == current_user.id:
+        form = CreateOrEditCourseForm(current_user, course.name, course.lms_id)
+        if form.validate_on_submit():
+
+            if not form.is_more_then_one_block.data:
                 num_of_blocks = 1
             else:
-                num_of_blocks = form.number_of_blocks.data
+                if form.number_of_blocks.data is None:
+                    num_of_blocks = 1
+                else:
+                    num_of_blocks = form.number_of_blocks.data
 
-        db.session.execute(update(Course).where(Course.id == course_id).values(name=form.name.data, lms_id=form.lms_id.data,
-                                                                               trainer_lms_id=form.trainer_lms_id.data,
-                                                                               trainer_telegram_id=form.trainer_telegram_id.data,
-                                                                               num_of_blocks=num_of_blocks,
-                                                                               is_certificate_needed=form.is_certificate_needed.data,
-                                                                               default_num_days=form.default_number_of_days.data))
-        db.session.commit()
-        flash('Данные курса были успешно изменены!')
-        return redirect(url_for('main.index'))
-    elif request.method == 'GET':
-        form.name.data = course.name
-        form.lms_id.data = course.lms_id
-        form.trainer_lms_id.data = course.trainer_lms_id
-        form.trainer_telegram_id.data = course.trainer_telegram_id
-        form.is_certificate_needed.data = course.is_certificate_needed
-        form.default_number_of_days.data = course.default_num_days
+            db.session.execute(update(Course).where(Course.id == course_id).values(name=form.name.data, lms_id=form.lms_id.data,
+                                                                                   trainer_lms_id=form.trainer_lms_id.data,
+                                                                                   trainer_telegram_id=form.trainer_telegram_id.data,
+                                                                                   num_of_blocks=num_of_blocks,
+                                                                                   is_certificate_needed=form.is_certificate_needed.data,
+                                                                                   default_num_days=form.default_number_of_days.data))
+            db.session.commit()
+            flash('Данные курса были успешно изменены!')
+            return redirect(url_for('main.index'))
+        elif request.method == 'GET':
+            form.name.data = course.name
+            form.lms_id.data = course.lms_id
+            form.trainer_lms_id.data = course.trainer_lms_id
+            form.trainer_telegram_id.data = course.trainer_telegram_id
+            form.is_certificate_needed.data = course.is_certificate_needed
+            form.default_number_of_days.data = course.default_num_days
 
-        if course.num_of_blocks == 1:
-            form.is_more_then_one_block.data = False
-        else:
-            form.is_more_then_one_block.data = True
-            form.number_of_blocks.data = course.num_of_blocks
+            if course.num_of_blocks == 1:
+                form.is_more_then_one_block.data = False
+            else:
+                form.is_more_then_one_block.data = True
+                form.number_of_blocks.data = course.num_of_blocks
 
-    return render_template('coursecreate/editcreate.html', title='Редактировать курс', form=form)
+        return render_template('coursecreate/editcreate.html', title='Редактировать курс', form=form)
+    else:
+        return render_template('error/403.html', title='Ошибка доступа')
 
 
 @bp.route('/delete', methods=['POST'])
