f04518befd5cc1a8464d549e0086ae0a8a83c2d3
Баранцев Степан <stepan.barantsev@gmail.com>
2020-09-20 17:31:14 +0300

    Текст на кнопке после клика меняется на противоположный

--- web/app/static/addEditStudent.js
+++ web/app/static/addEditStudent.js
@@ -1,8 +1,20 @@
 $(document).ready(function() {
     $("#unlock_fields").click(function() {
-        $("#lms_email_locked").removeAttr("readonly")
-        $("#telegram_id_locked").removeAttr("readonly")
-        $("#name_locked").removeAttr("readonly")
-        $("#registration_code_locked").removeAttr("readonly")
+        if ($("#unlock_fields").attr("data-flag") == "True") {
+            $("#unlock_fields").attr("data-flag", "False")
+            $("#unlock_fields").text("Запретить менять поля")
+            $("#lms_email_locked").removeAttr("readonly")
+            $("#telegram_id_locked").removeAttr("readonly")
+            $("#name_locked").removeAttr("readonly")
+            $("#registration_code_locked").removeAttr("readonly")
+        }
+        else {
+            $("#unlock_fields").attr("data-flag", "True")
+            $("#unlock_fields").text("Разрешить менять заблокированные поля")
+            $("#lms_email_locked").prop("readonly",true)
+            $("#telegram_id_locked").prop("readonly",true)
+            $("#name_locked").prop("readonly",true)
+            $("#registration_code_locked").prop("readonly",true)
+        }
     });
 });
