8cf9cea320f48208caec4a8078b2bc65dc5570aa
Баранцев Степан <stepan.barantsev@gmail.com>
2021-01-17 18:29:52 +0300

    Сделал преобразование даты

--- api_helper/fauna_helper.py
+++ api_helper/fauna_helper.py
@@ -14,12 +14,31 @@ class FaunaHelper:
                                                   {"data": {
                                                       "student_name": student.name,
                                                       "email": student.email,
-                                                      "date": "15 июля 2019",
+                                                      "date": FaunaHelper.create_current_date_in_specific_format(),
                                                       "course_name": student.course.name,
                                                       "otl": False}
                                                   }))['ref'].value['id']
 
-
     @staticmethod
     def create_current_date_in_specific_format():
-        current_date = None
+        current_date = datetime.date.today()
+        current_date = str(current_date).split('-')
+        return f'{int(current_date[2])} {FaunaHelper.replace_number_date_to_string(current_date[1])} {int(current_date[0])}'
+
+    @staticmethod
+    def replace_number_date_to_string(number_date):
+        d = {
+            "01": "января",
+            "02": "февраля",
+            "03": "марта",
+            "04": "апреля",
+            "05": "мая",
+            "06": "июня",
+            "07": "июля",
+            "08": "августа",
+            "09": "сентября",
+            "10": "октября",
+            "11": "ноября",
+            "12": "декабря"
+        }
+        return d[number_date]
