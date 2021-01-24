4ea91af903260805f1f29d0a20fca317152f9bcd
Баранцев Степан <stepan.barantsev@gmail.com>
2021-01-24 17:34:51 +0300

    Доставлено/не доставлено

--- telegram/schedule/main.py
+++ telegram/schedule/main.py
@@ -21,15 +21,24 @@ def job():
 
             for student in students:
                 student.number_of_days -= 1
-                message_about_days += f'У студента {student.name} осталось {student.number_of_days} дней.\n'
-                send_message_about_days_to_student(student)
+                is_delivered = send_message_about_days_to_student(student)
+
+                if is_delivered:
+                    message_about_days += f'У студента {student.name} осталось {student.number_of_days} дней. (Доставлено)\n'
+                else:
+                    message_about_days += f'У студента {student.name} осталось {student.number_of_days} дней. (Не доставлено)\n'
                 if course.is_certificate_needed:
                     if student.cert_link is None and student.status != 'finished':
                         cert_link = try_to_generate_cert_to_student(student)
-                        send_message_about_certificate(student.telegram_id, cert_link, discount_coupon, student)
-                        send_message_about_certificate(course.author.telegram_id, cert_link, discount_coupon, student)
+                        if cert_link is not None:
+                            is_delivered = send_message_about_certificate(student.telegram_id, cert_link, discount_coupon, student)
+                            send_message_about_certificate(course.author.telegram_id, cert_link, discount_coupon, student, is_delivered)
 
-            bot.send_message(course.author.telegram_id, message_about_days)
+            try:
+                bot.send_message(course.author.telegram_id, message_about_days)
+            except:
+                pass
+
             session.commit()
 
 
@@ -38,17 +47,32 @@ def send_message_about_days_to_student(student):
         if student.number_of_days % 10 == 0:
             course_name_and_author = f'{student.course.name} [{student.course.author.name}]'
             bot.send_message(student.telegram_id, get_message_with_course_prefix('NUM_OF_DAYS_SCHEDULED', None, student.number_of_days, course_name=course_name_and_author))
+            return True
+        return False
     except ApiTelegramException:
-        pass
+        return False
 
 
-def send_message_about_certificate(telegram_id, cert_link, discount_coupon, student):
+def send_message_about_certificate(telegram_id, cert_link, discount_coupon, student, is_delivered=None):
     try:
-        if cert_link is not None:
-            course_name_and_author = f'{student.course.name} [{student.course.author.name}]'
+        course_name_and_author = f'{student.course.name} [{student.course.author.name}]'
+        if is_delivered is None:
             bot.send_message(telegram_id, get_message_with_course_prefix('CERTIFICATE', None, cert_link, discount_coupon, student.course.review_link, course_name=course_name_and_author))
+        # Сообщение для теренера о том, доставлено ли студенту сообщение
+        else:
+            if is_delivered:
+                bot.send_message(telegram_id,
+                                 get_message_with_course_prefix('CERTIFICATE', None, cert_link, discount_coupon,
+                                                                student.course.review_link,
+                                                                course_name=course_name_and_author) + ' (Доставлено)')
+            else:
+                bot.send_message(telegram_id,
+                                 get_message_with_course_prefix('CERTIFICATE', None, cert_link, discount_coupon,
+                                                                student.course.review_link,
+                                                                course_name=course_name_and_author) + ' (Не доставлено)')
+        return True
     except ApiTelegramException:
-        pass
+        return False
 
 
 def try_to_generate_cert_to_student(student):
