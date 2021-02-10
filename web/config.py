5f55bec70f02975c8a87c445f406edb7042d064b
Баранцев Степан <stepan.barantsev@gmail.com>
2021-02-11 01:31:43 +0300

    Изменил код конфига для работы с булевыми переменными

--- web/config.py
+++ web/config.py
@@ -8,8 +8,8 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT'))
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS')
-    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL')
+    MAIL_USE_TLS = True if os.environ.get('MAIL_USE_TLS') == 'True' else False
+    MAIL_USE_SSL =  True if os.environ.get('MAIL_USE_SSL') == 'True' else False
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = [os.environ.get('ADMIN')]
