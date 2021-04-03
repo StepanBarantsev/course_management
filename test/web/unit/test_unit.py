d550af58c60dcb28b7e01505c73482082270a528
Баранцев Степан <stepan.barantsev@gmail.com>
2021-04-04 01:20:39 +0300

    Второй рабочий тест + исправление в 1

--- test/web/unit/test_unit.py
+++ test/web/unit/test_unit.py
@@ -1,8 +1,7 @@
-from test.web.unit.helpers import login, logout
 from web.app.models import User
 
 
-def test_register(app):
+def test_registration_success(app):
 
     username = 'testUser'
     email = 'testmail@gmail.com'
@@ -21,11 +20,31 @@ def test_register(app):
 
     assert response.status == '200 OK'
     assert len(users) == 1
-    assert user.username == email
+    assert user.username == username
     assert user.name is None
-    assert user.email == 'testmail@gmail.com'
+    assert user.email == email
     assert user.telegram_id is None
     assert user.telegram_nickname is None
     assert user.lms_id is None
     assert user.flag_emails_from_default_mail is False
     assert user.flag_is_messages_from_bot_is_delivered is True
+
+
+def test_registration_fail(app):
+
+    username = 'testUser'
+    email = 'testmail@gmail.com'
+
+    response = app['client'].post('/auth/register', data=dict(
+                username=username,
+                email=email,
+                password='123',
+                password2='123',
+                registration_code='incorrectCode',
+                submit="Зарегистрироваться"
+            ), follow_redirects=True)
+
+    users = User.get_all_users()
+
+    assert response.status == '403 FORBIDDEN'
+    assert len(users) == 0
