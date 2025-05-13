c7f0ae656cb061327c8611246b965ee25b25e8cf
Stepan Barantsev <stepan.barantsev@gmail.com>
2025-05-13 04:09:03 +0300

    Update config.py

--- telegram/config.py
+++ telegram/config.py
@@ -7,7 +7,14 @@ class ConfigTelegram:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     DEFAULT_MAIL = os.environ.get('ADMIN')
 
-    CERT_MAIL_PORT = int(os.environ.get('CERT_MAIL_PORT'))
-    CERT_MAIL_SERVER = os.environ.get('CERT_MAIL_SERVER')
-    CERT_MAIL_USERNAME = os.environ.get('CERT_MAIL_USERNAME')
-    CERT_MAIL_PASSWORD = os.environ.get('CERT_MAIL_PASSWORD')
+    MAIL_ADDRESS = os.getenv("MAIL_ADDRESS")
+    MAIL_KEY = os.getenv("MAIL_KEY")
+    CERT_MAIL_ADDR = os.getenv("CERT_MAIL_ADDR")
+
+    COUPON_CODE = os.environ.get('COUPON_CODE')
+
+    CERT_DB_HOST = os.getenv("CERT_DB_HOST")
+    CERT_DB_PORT = os.getenv("CERT_DB_PORT")
+    CERT_DB_NAME = os.getenv("CERT_DB_NAME")
+    CERT_DB_USER = os.getenv("CERT_DB_USER")
+    CERT_DB_PASSWORD = os.getenv("CERT_DB_PASSWORD")
