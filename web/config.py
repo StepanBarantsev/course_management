8a6b85a7f5cca25ca4cc4c0f2d3b41e1df42fe3a
Stepan Barantsev <stepan.barantsev@gmail.com>
2025-05-13 04:18:55 +0300

    Update config.py

--- web/config.py
+++ web/config.py
@@ -6,12 +6,6 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT'))
-    MAIL_USE_TLS = True if os.environ.get('MAIL_USE_TLS') == 'True' else False
-    MAIL_USE_SSL = True if os.environ.get('MAIL_USE_SSL') == 'True' else False
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = [os.environ.get('ADMIN')]
     SECURITY_EMAIL_SENDER = os.environ.get('ADMIN')
 
